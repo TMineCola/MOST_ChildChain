a8c30c06b99686fb876f8a50dba15f2ad941f1fb
Wang Jiun Jie <wang.minecola@gmail.com>
2020-06-11 13:20:55 +0800

    Child chain deploy only crosschain_tx

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,4 @@
 const crosschain_tx = artifacts.require("CrosschainTransaction.sol");
-const nameServer = artifacts.require("NamingService.sol");
 module.exports = function(deployer) {
   deployer.deploy(crosschain_tx);
-  deployer.deploy(nameServer);
 };
